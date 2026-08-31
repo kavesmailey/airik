d400f200dea25d04c5cb4f6f54b2cb07d0a2a034
kavesmailey <kavesmailey@gmail.com>
2026-08-31 16:49:57 +0330

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+import Header from "@/components/layout/Header";
+import Footer from "@/components/layout/Footer";
 
 const siteUrl =
   process.env.NEXT_PUBLIC_SITE_URL || "https://airik-xi.vercel.app";
