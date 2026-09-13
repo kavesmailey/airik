1a26b2f2a0097ed5062a4b0bc515f1adf4a6973e
kavesmailey <kavesmailey@gmail.com>
2026-09-13 10:33:14 +0330

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -2,14 +2,13 @@ import type { MetadataRoute } from "next";
 
 import { siteConfig } from "@/content/site";
 
-const baseUrl = siteConfig.siteUrl;
+const baseUrl = siteConfig.siteUrl.replace(/\/$/, "");
 
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: ["/api/", "/_next/"],
     },
     sitemap: `${baseUrl}/sitemap.xml`,
     host: baseUrl,
